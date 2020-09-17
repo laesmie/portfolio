eb85b7e078765dd044c29c2568bf2c272ff447db
laesmie <kylaleeson@gmail.com>
2020-09-17 11:37:55 -0700

    Changes

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var document;
-var today = new Date();
-var y = today.getFullYear();
-document.getElementById('current-year').innerHTML = y;
-
-var update = document.lastModified;
-document.getElementById('update-time').innerHTML = update;
